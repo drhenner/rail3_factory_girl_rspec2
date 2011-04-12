972b1371bcdf753c35c16065dcba2aa924dc5284
David Henner <drhenner@yahoo.com>
2011-04-11 23:01:05 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorBlog::Application.config.secret_token = 'b1dc75257543fa72d02f9f67a9f13b0312f526e661ce81446bf09ae16802be6fceb595bfffa0cba8efd0d9d90cf6030359a57e01881a55d54dad8a698d998ecb'
